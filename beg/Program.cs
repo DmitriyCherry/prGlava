954e0a1f6a2240178cc8e8b513df6bb531eec075
Dmitriy <dmitr.wischnewski@mail.ru>
2023-02-07 14:21:27 +0300

    First task is ready

---
+++ beg/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите число, которое нужно возвести в квадрат: ");
+double value = double.Parse(Console.ReadLine()!);
+double result = Math.Pow(value, 2);
+Console.WriteLine($"Квадрат числа {value}, возведенного в квадрат, равен: {result}");
