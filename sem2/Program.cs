95e91dc07cdaa64d6c984e9d62b2beb4cb2fcf83
Dmitriy <dmitr.wischnewski@mail.ru>
2023-02-08 20:23:24 +0300

    Трёхзначное число с лишней второй цифрой

---
+++ sem2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую
+// цифру этого числа.
+
+int randomNumber = new Random().Next(100, 1000);
+int firstDigit = randomNumber / 100;
+
+int firstTwoDigit = randomNumber / 10;
+int secondDigit = firstTwoDigit % 10;
+
+int thirdDigit = randomNumber % 10;
+
+Console.Write($"Вывод крайних цифр из случайно сгенерированного трёхзначного числа {randomNumber}: {firstDigit}{thirdDigit}");
