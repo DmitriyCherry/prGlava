a486b1bb8d17fd6100e39801ddd8b99261e07ac0
Dmitriy <dmitr.wischnewski@mail.ru>
2023-02-09 12:24:37 +0300

    Расстояние 2d

--- sem3/Program.cs
+++ sem3/Program.cs
@@ -2,7 +2,7 @@
 // координат точек в этой четверти (x и y).
 
 
-Console.Write("Введите номер четверти: ");
+/*Console.Write("Введите номер четверти: ");
 int num = int.Parse(Console.ReadLine()!);
 
 if (num == 1)
@@ -20,4 +20,29 @@ if (num == 1)
 }else
 {
     Console.WriteLine("Такой четверти нет");
-}
+}*/
+
+
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между
+// ними в 2D-пространстве.
+
+
+Console.WriteLine("Введите координаты для точки A: ");
+Console.Write("Координата точки A по оси абсцисс: ");
+double ax =  double.Parse(Console.ReadLine()!);
+Console.Write("Координата точки A по оси ординат: ");
+double ay =  double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите координаты для точки B: ");
+Console.Write("Координата точки B по оси абсцисс: ");
+double bx =  double.Parse(Console.ReadLine()!);
+Console.Write("Координата точки B по оси ординат: ");
+double by =  double.Parse(Console.ReadLine()!);
+
+double bc = Math.Pow(ax-bx, 2);
+double ac = Math.Pow(ay-by, 2);
+
+double result = Math.Sqrt(bc + ac);
+double roundResult = Math.Round(result, 2);
+Console.WriteLine($"Расстояние между точками будет равно: {roundResult}");
