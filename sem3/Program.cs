fd0d14d04c61f61a5b51b7c38034a8076943beb1
Dmitriy <dmitr.wischnewski@mail.ru>
2023-02-09 11:31:54 +0300

    Четверти и диапазоны

---
+++ sem3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает какой диапазон возможных
+// координат точек в этой четверти (x и y).
+
+
+Console.Write("Введите номер четверти: ");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num == 1)
+{
+    Console.WriteLine("Все точки лежат в диапазоне (x, y)");
+}else if (num == 2)
+{
+    Console.WriteLine("Все точки лежат в диапазоне (-x, y)");
+}else if (num == 3)
+{
+    Console.WriteLine("Все точки лежат в диапазоне (-x, -y)");
+}else if (num == 4)
+{
+    Console.WriteLine("Все точки лежат в диапазоне (x, -y)");
+}else
+{
+    Console.WriteLine("Такой четверти нет");
+}
